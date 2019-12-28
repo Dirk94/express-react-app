453b47c2fd67375ebacc48534f0b372f97c67a40
Dirk Hoekstra <dirkhoekstra@gmail.com>
2019-12-28 14:46:13 +0100

    Serve the frontend application from Express

--- app/Server.ts
+++ app/Server.ts
@@ -1,4 +1,6 @@
 import {Express, Request, Response} from "express";
+import express from "express";
+import * as path from "path";
 
 export class Server {
 
@@ -7,9 +9,15 @@ export class Server {
     constructor(app: Express) {
         this.app = app;
 
+        this.app.use(express.static(path.resolve("./") + "/build/frontend"));
+
         this.app.get("/api", (req: Request, res: Response): void => {
             res.send("You have reached the API!");
-        })
+        });
+
+        this.app.get("*", (req: Request, res: Response): void => {
+            res.sendFile(path.resolve("./") + "/build/frontend/index.html");
+        });
     }
 
     public start(port: number): void {
